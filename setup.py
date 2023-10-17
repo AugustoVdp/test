6a5ffc1de0214915cd68ef99132a7086b9061835
AugustoVdp <vanderploeg.augustototo@gmail.com>
2023-10-17 14:50:34 -0300

    carga

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("DESCRIPTION.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='setuptools_ejercicio',
+    name='test',
     version='0.1.0',
     author='Augusto Van der Ploeg',
     author_email='vanderploeg.augusto@gmail.com',
     description='Ejercicio de setuptools',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/AugustoVdp/setuptools_ejercicio.git',
+    url='https://github.com/AugustoVdp/test.git',
     project_urls = {
-        "Bug Tracker": "https://github.com/AugustoVdp/setuptools_ejercicio/issues"
+        "Bug Tracker": "https://github.com/AugustoVdp/test/issues"
     },
     license='MIT',
     packages=['test'],
